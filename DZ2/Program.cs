84f067251ff6ff2b218fce93811d1cf9a01a87b0
Nimere1990 <nimereiv@gmail.com>
2022-09-28 18:27:27 +0300

    Готовое ДЗ

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -4,3 +4,16 @@
 82 -> 10
 9012 -> 12
 */
+
+Console.WriteLine("Введите число: ");
+int i = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+    int num = i % 10;
+    i = i / 10;
+    sum = sum + num;
+}
+
+Console.WriteLine("Cумма цифр в числе равна: " + sum);
--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -4,3 +4,15 @@
 6, 1, 33 -> [6, 1, 33]
 ввод элеменетов через консоль (через запятую. использовать метод string.Split())
 */
+
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine());
+int[] massiv = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите {0}-й элемент", i + 1);
+    massiv[i] = int.Parse(Console.ReadLine());
+}
+
+var result = string.Join(", ", massiv);
+Console.Write($"Массив: [{result}]");
